213c9b1485e44400b782bb2617b8c88809f1c753
damian <ivan_damianv2@outlook.es>
2022-07-14 12:36:31 -0500

    Cambio de login

---
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <style>
+        body{margin: 0;}
+        .container{
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            background:white;
+            background-size: cover;
+            height: 100vh ;
+        }
+        .left{
+            background: white;
+            display: flex ;
+            justify-content: center;
+            place-items: center;
+            text-align: left;
+        }
+        .left h1{
+            color: black; font-size: 20px;
+        }
+
+        .right{
+            display: flex ;
+            justify-content: center;
+            place-items: center;
+            text-align: left;
+        }
+
+        @media(max-width:900px){
+            .container{
+                grid-template-columns: 3fr 1fr;
+            }
+        }
+
+    </style>
+
+</head>
+<body>
+
+
+    <div class="container">
+        <div class="left">
+
+            <div>
+                <img width="350px" src="{{ asset('img/logo7.png') }}" alt="">
+                <br>
+                <br>
+                <h1 > <strong>Ingresa a tu cuenta</strong> </h1>
+                <br>
+                {{ $slot }}
+            </div>
+
+
+
+        </div>
+
+        <div class="right">
+            <img width="625px" src="{{ asset('img/inicio_login1.png') }}" alt="">
+
+        </div>
+    </div>
+</body>
+</html>
+
