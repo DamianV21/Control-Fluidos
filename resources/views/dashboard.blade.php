f069abe3c21a6c7b0aa2481a685205f6e289f260
CoderID <Ivan_DamianV2@outlook.es>
2022-09-07 10:07:38 -0500

    Modificacion nombre de tecnico

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,6 +20,8 @@
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
+    <link rel="stylesheet" href="css/grafcias.css">
+
 </head>
 
 <body id="page-top">
@@ -120,8 +122,10 @@
 
             <!-- Sidebar Message -->
             <div class="sidebar-card d-none d-lg-flex">
-                <img class="sidebar-card-illustration mb-2" src="{{ asset('img/undraw_upload_re_pasx.svg') }}" alt="admin">
-                <p class="text-center mb-2"><strong>Sistema Administrativo</strong> dirigete al panel para administrar tu información.</p>
+                <img class="sidebar-card-illustration mb-2" src="{{ asset('img/undraw_upload_re_pasx.svg') }}"
+                    alt="admin">
+                <p class="text-center mb-2"><strong>Sistema Administrativo</strong> dirigete al panel para administrar
+                    tu información.</p>
                 <a class="btn btn-success btn-sm" href="admin">Ingresar</a>
             </div>
 
@@ -471,43 +475,6 @@
                     </div>
 
                     <!-- Content Row -->
-
-                    <div class="row">
-
-                        <!-- Area Chart -->
-                        <div class="col-xl-8 col-lg-7">
-                            <div class="card shadow mb-4">
-                                <!-- Card Header - Dropdown -->
-                                <div
-                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Ultimas concentraciones</h6>
-
-                                </div>
-                                <!-- Card Body -->
-                                <div class="card-body">
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Pie Chart -->
-                        <div class="col-xl-4 col-lg-5">
-                            <div class="card shadow mb-4">
-                                <!-- Card Header - Dropdown -->
-                                <div
-                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary"></h6>
-
-                                </div>
-                                <!-- Card Body -->
-                                <div class="card-body">
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Content Row -->
                     <div class="row">
 
                         <!-- Content Column -->
@@ -591,7 +558,7 @@
                                                     <img class="d-block w-100" src="{{ asset('img/fondo.png') }}"
                                                         alt="Second slide">
                                                     <div class="carousel-caption d-none d-md-block">
-                                                        <h5 > Nuevo comentario</h5>
+                                                        <h5> Nuevo comentario</h5>
                                                         <p>Esta es una descripcion del comentario</p>
                                                     </div>
                                                 </div>
@@ -635,6 +602,47 @@
                         </div>
                     </div>
 
+                    <!-- Content Row -->
+                    <div class="row">
+
+                        <!-- Area Chart -->
+                        <div class="col-xl-8 col-lg-7">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Ultimas concentraciones</h6>
+
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+
+                                    <figure class="highcharts-figure">
+                                        <div id="graficas"></div>
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Pie Chart -->
+                        <div class="col-xl-4 col-lg-5">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary"></h6>
+
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
                 </div>
                 <!-- ================================== /.container-fluid ============================================= -->
 
@@ -673,14 +681,17 @@
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Seleccione "Cerrar sesión" a continuación si está listo para finalizar su sesión actual.</div>
+                <div class="modal-body">Seleccione "Cerrar sesión" a continuación si está listo para finalizar su
+                    sesión actual.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
                         <div class="nav-item">
-                            <a class="btn btn-primary" href="{{ route('logout') }}" onclick="event.preventDefault();
-                        this.closest('form').submit(); " role="button">Cerrar Sesión</a>
+                            <a class="btn btn-primary" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                        this.closest('form').submit(); "
+                                role="button">Cerrar Sesión</a>
                         </div>
                     </form>
 
@@ -707,6 +718,273 @@
     <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
+
+    <!-- highcharts Graficas -->
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/series-label.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+
+    <script>
+        Highcharts.chart('graficas', {
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: 'Snow depth at Vikjafjellet, Norway'
+            },
+            subtitle: {
+                text: 'Irregular time data in Highcharts JS'
+            },
+            xAxis: {
+                type: 'datetime',
+                dateTimeLabelFormats: { // don't display the dummy year
+                    month: '%e. %b',
+                    year: '%b'
+                },
+                title: {
+                    text: 'Date'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Snow depth (m)'
+                },
+                min: 0
+            },
+            tooltip: {
+                headerFormat: '<b>{series.name}</b><br>',
+                pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+            },
+
+            plotOptions: {
+                series: {
+                    marker: {
+                        enabled: true,
+                        radius: 2.5
+                    }
+                }
+            },
+
+            colors: ['#6CF', '#39F', '#06C', '#036', '#000'],
+
+            // Define the data points. All series have a dummy year of 1970/71 in order
+            // to be compared on the same x axis. Note that in JavaScript, months start
+            // at 0 for January, 1 for February etc.
+            series: [{
+                name: "Winter 2019-2020",
+                data: [
+                    [Date.UTC(1970, 9, 24), 0],
+                    [Date.UTC(1970, 9, 27), 0.12],
+                    [Date.UTC(1970, 9, 30), 0.09],
+                    [Date.UTC(1970, 10, 3), 0.13],
+                    [Date.UTC(1970, 10, 6), 0.12],
+                    [Date.UTC(1970, 10, 9), 0.13],
+                    [Date.UTC(1970, 10, 12), 0.13],
+                    [Date.UTC(1970, 10, 15), 0.16],
+                    [Date.UTC(1970, 10, 18), 0.19],
+                    [Date.UTC(1970, 10, 21), 0.25],
+                    [Date.UTC(1970, 10, 24), 0.26],
+                    [Date.UTC(1970, 10, 27), 0.24],
+                    [Date.UTC(1970, 10, 30), 0.25],
+                    [Date.UTC(1970, 11, 3), 0.26],
+                    [Date.UTC(1970, 11, 6), 0.36],
+                    [Date.UTC(1970, 11, 9), 0.43],
+                    [Date.UTC(1970, 11, 12), 0.32],
+                    [Date.UTC(1970, 11, 15), 0.48],
+                    [Date.UTC(1970, 11, 18), 0.5],
+                    [Date.UTC(1970, 11, 21), 0.44],
+                    [Date.UTC(1970, 11, 24), 0.43],
+                    [Date.UTC(1970, 11, 27), 0.45],
+                    [Date.UTC(1970, 11, 30), 0.4],
+                    [Date.UTC(1971, 0, 3), 0.39],
+                    [Date.UTC(1971, 0, 6), 0.56],
+                    [Date.UTC(1971, 0, 9), 0.57],
+                    [Date.UTC(1971, 0, 12), 0.68],
+                    [Date.UTC(1971, 0, 15), 0.93],
+                    [Date.UTC(1971, 0, 18), 1.11],
+                    [Date.UTC(1971, 0, 21), 1.01],
+                    [Date.UTC(1971, 0, 24), 0.99],
+                    [Date.UTC(1971, 0, 27), 1.17],
+                    [Date.UTC(1971, 0, 30), 1.24],
+                    [Date.UTC(1971, 1, 3), 1.41],
+                    [Date.UTC(1971, 1, 6), 1.47],
+                    [Date.UTC(1971, 1, 9), 1.4],
+                    [Date.UTC(1971, 1, 12), 1.92],
+                    [Date.UTC(1971, 1, 15), 2.03],
+                    [Date.UTC(1971, 1, 18), 2.46],
+                    [Date.UTC(1971, 1, 21), 2.53],
+                    [Date.UTC(1971, 1, 24), 2.73],
+                    [Date.UTC(1971, 1, 27), 2.67],
+                    [Date.UTC(1971, 2, 3), 2.65],
+                    [Date.UTC(1971, 2, 6), 2.62],
+                    [Date.UTC(1971, 2, 9), 2.79],
+                    [Date.UTC(1971, 2, 13), 2.93],
+                    [Date.UTC(1971, 2, 20), 3.09],
+                    [Date.UTC(1971, 2, 27), 2.76],
+                    [Date.UTC(1971, 2, 30), 2.73],
+                    [Date.UTC(1971, 3, 4), 2.9],
+                    [Date.UTC(1971, 3, 9), 2.77],
+                    [Date.UTC(1971, 3, 12), 2.78],
+                    [Date.UTC(1971, 3, 15), 2.76],
+                    [Date.UTC(1971, 3, 18), 2.76],
+                    [Date.UTC(1971, 3, 21), 2.7],
+                    [Date.UTC(1971, 3, 24), 2.61],
+                    [Date.UTC(1971, 3, 27), 2.52],
+                    [Date.UTC(1971, 3, 30), 2.53],
+                    [Date.UTC(1971, 4, 3), 2.55],
+                    [Date.UTC(1971, 4, 6), 2.52],
+                    [Date.UTC(1971, 4, 9), 2.44],
+                    [Date.UTC(1971, 4, 12), 2.43],
+                    [Date.UTC(1971, 4, 15), 2.43],
+                    [Date.UTC(1971, 4, 18), 2.48],
+                    [Date.UTC(1971, 4, 21), 2.41],
+                    [Date.UTC(1971, 4, 24), 2.16],
+                    [Date.UTC(1971, 4, 27), 2.01],
+                    [Date.UTC(1971, 4, 30), 1.88],
+                    [Date.UTC(1971, 5, 2), 1.62],
+                    [Date.UTC(1971, 5, 6), 1.43],
+                    [Date.UTC(1971, 5, 9), 1.3],
+                    [Date.UTC(1971, 5, 12), 1.11],
+                    [Date.UTC(1971, 5, 15), 0.84],
+                    [Date.UTC(1971, 5, 18), 0.54],
+                    [Date.UTC(1971, 5, 21), 0.19],
+                    [Date.UTC(1971, 5, 23), 0]
+                ]
+            }, {
+                name: "Winter 2020-2021",
+                data: [
+                    [Date.UTC(1970, 10, 14), 0],
+                    [Date.UTC(1970, 11, 6), 0.35],
+                    [Date.UTC(1970, 11, 13), 0.35],
+                    [Date.UTC(1970, 11, 20), 0.33],
+                    [Date.UTC(1970, 11, 30), 0.53],
+                    [Date.UTC(1971, 0, 13), 0.62],
+                    [Date.UTC(1971, 0, 20), 0.6],
+                    [Date.UTC(1971, 1, 2), 0.69],
+                    [Date.UTC(1971, 1, 18), 0.67],
+                    [Date.UTC(1971, 1, 21), 0.65],
+                    [Date.UTC(1971, 1, 24), 0.66],
+                    [Date.UTC(1971, 1, 27), 0.66],
+                    [Date.UTC(1971, 2, 3), 0.61],
+                    [Date.UTC(1971, 2, 6), 0.6],
+                    [Date.UTC(1971, 2, 9), 0.69],
+                    [Date.UTC(1971, 2, 12), 0.66],
+                    [Date.UTC(1971, 2, 15), 0.75],
+                    [Date.UTC(1971, 2, 18), 0.76],
+                    [Date.UTC(1971, 2, 21), 0.75],
+                    [Date.UTC(1971, 2, 24), 0.69],
+                    [Date.UTC(1971, 2, 27), 0.82],
+                    [Date.UTC(1971, 2, 30), 0.86],
+                    [Date.UTC(1971, 3, 3), 0.81],
+                    [Date.UTC(1971, 3, 6), 1],
+                    [Date.UTC(1971, 3, 9), 1.15],
+                    [Date.UTC(1971, 3, 10), 1.35],
+                    [Date.UTC(1971, 3, 12), 1.26],
+                    [Date.UTC(1971, 3, 15), 1.18],
+                    [Date.UTC(1971, 3, 18), 1.14],
+                    [Date.UTC(1971, 3, 21), 1.04],
+                    [Date.UTC(1971, 3, 24), 1.06],
+                    [Date.UTC(1971, 3, 27), 1.05],
+                    [Date.UTC(1971, 3, 30), 1.03],
+                    [Date.UTC(1971, 4, 3), 1.01],
+                    [Date.UTC(1971, 4, 6), 0.98],
+                    [Date.UTC(1971, 4, 9), 0.94],
+                    [Date.UTC(1971, 4, 12), 0.8],
+                    [Date.UTC(1971, 4, 15), 0.61],
+                    [Date.UTC(1971, 4, 18), 0.43],
+                    [Date.UTC(1971, 4, 21), 0.29],
+                    [Date.UTC(1971, 4, 24), 0.1],
+                    [Date.UTC(1971, 4, 26), 0]
+                ]
+            }, {
+                name: "Winter 2021-2022",
+                data: [
+                    [Date.UTC(1970, 10, 5), 0],
+                    [Date.UTC(1970, 10, 12), 0.1],
+                    [Date.UTC(1970, 10, 21), 0.15],
+                    [Date.UTC(1970, 10, 22), 0.19],
+                    [Date.UTC(1970, 10, 27), 0.17],
+                    [Date.UTC(1970, 10, 30), 0.27],
+                    [Date.UTC(1970, 11, 2), 0.25],
+                    [Date.UTC(1970, 11, 4), 0.27],
+                    [Date.UTC(1970, 11, 5), 0.26],
+                    [Date.UTC(1970, 11, 6), 0.25],
+                    [Date.UTC(1970, 11, 7), 0.26],
+                    [Date.UTC(1970, 11, 8), 0.26],
+                    [Date.UTC(1970, 11, 9), 0.25],
+                    [Date.UTC(1970, 11, 10), 0.25],
+                    [Date.UTC(1970, 11, 11), 0.25],
+                    [Date.UTC(1970, 11, 12), 0.26],
+                    [Date.UTC(1970, 11, 22), 0.22],
+                    [Date.UTC(1970, 11, 23), 0.22],
+                    [Date.UTC(1970, 11, 24), 0.22],
+                    [Date.UTC(1970, 11, 25), 0.24],
+                    [Date.UTC(1970, 11, 26), 0.24],
+                    [Date.UTC(1970, 11, 27), 0.24],
+                    [Date.UTC(1970, 11, 28), 0.24],
+                    [Date.UTC(1970, 11, 29), 0.24],
+                    [Date.UTC(1970, 11, 30), 0.22],
+                    [Date.UTC(1970, 11, 31), 0.18],
+                    [Date.UTC(1971, 0, 1), 0.17],
+                    [Date.UTC(1971, 0, 2), 0.23],
+                    [Date.UTC(1971, 0, 9), 0.5],
+                    [Date.UTC(1971, 0, 10), 0.5],
+                    [Date.UTC(1971, 0, 11), 0.53],
+                    [Date.UTC(1971, 0, 12), 0.48],
+                    [Date.UTC(1971, 0, 13), 0.4],
+                    [Date.UTC(1971, 0, 17), 0.36],
+                    [Date.UTC(1971, 0, 22), 0.69],
+                    [Date.UTC(1971, 0, 23), 0.62],
+                    [Date.UTC(1971, 0, 29), 0.72],
+                    [Date.UTC(1971, 1, 2), 0.95],
+                    [Date.UTC(1971, 1, 10), 1.73],
+                    [Date.UTC(1971, 1, 15), 1.76],
+                    [Date.UTC(1971, 1, 26), 2.18],
+                    [Date.UTC(1971, 2, 2), 2.22],
+                    [Date.UTC(1971, 2, 6), 2.13],
+                    [Date.UTC(1971, 2, 8), 2.11],
+                    [Date.UTC(1971, 2, 9), 2.12],
+                    [Date.UTC(1971, 2, 10), 2.11],
+                    [Date.UTC(1971, 2, 11), 2.09],
+                    [Date.UTC(1971, 2, 12), 2.08],
+                    [Date.UTC(1971, 2, 13), 2.08],
+                    [Date.UTC(1971, 2, 14), 2.07],
+                    [Date.UTC(1971, 2, 15), 2.08],
+                    [Date.UTC(1971, 2, 17), 2.12],
+                    [Date.UTC(1971, 2, 18), 2.19],
+                    [Date.UTC(1971, 2, 21), 2.11],
+                    [Date.UTC(1971, 2, 24), 2.1],
+                    [Date.UTC(1971, 2, 27), 1.89],
+                    [Date.UTC(1971, 2, 30), 1.92],
+                    [Date.UTC(1971, 3, 3), 1.9],
+                    [Date.UTC(1971, 3, 6), 1.95],
+                    [Date.UTC(1971, 3, 9), 1.94],
+                    [Date.UTC(1971, 3, 12), 2],
+                    [Date.UTC(1971, 3, 15), 1.9],
+                    [Date.UTC(1971, 3, 18), 1.84],
+                    [Date.UTC(1971, 3, 21), 1.75],
+                    [Date.UTC(1971, 3, 24), 1.69],
+                    [Date.UTC(1971, 3, 27), 1.64],
+                    [Date.UTC(1971, 3, 30), 1.64],
+                    [Date.UTC(1971, 4, 3), 1.58],
+                    [Date.UTC(1971, 4, 6), 1.52],
+                    [Date.UTC(1971, 4, 9), 1.43],
+                    [Date.UTC(1971, 4, 12), 1.42],
+                    [Date.UTC(1971, 4, 15), 1.37],
+                    [Date.UTC(1971, 4, 18), 1.26],
+                    [Date.UTC(1971, 4, 21), 1.11],
+                    [Date.UTC(1971, 4, 24), 0.92],
+                    [Date.UTC(1971, 4, 27), 0.75],
+                    [Date.UTC(1971, 4, 30), 0.55],
+                    [Date.UTC(1971, 5, 3), 0.35],
+                    [Date.UTC(1971, 5, 6), 0.21],
+                    [Date.UTC(1971, 5, 9), 0]
+                ]
+            }]
+        });
+    </script>
 
 </body>
 
--- resources/views/livewire/reportes/refrigerantes.blade.php
+++ resources/views/livewire/reportes/refrigerantes.blade.php
@@ -409,7 +409,7 @@
                                             {{ $da->aceites_entrampados }}</td>
                                     </tr>
                                     <tr>
-                                        <td colspan="6"><strong>Técnico:</strong> {{ $da->usuario_id}}</td>
+                                        <td colspan="6"><strong>Técnico:</strong> {{ $da->usuarios->name}}</td>
                                     </tr>
                                 </tbody>
                             </table>
