4c460e8b26f2819898b67b7c08cb5c6190f131e5
CoderID <Ivan_DamianV2@outlook.es>
2022-09-28 13:47:06 -0500

    Actualizacion de composer

--- vendor/tcg/voyager/resources/views/bread/edit-add.blade.php
+++ vendor/tcg/voyager/resources/views/bread/edit-add.blade.php
@@ -12,7 +12,6 @@
 @section('page_title', __('voyager::generic.'.($edit ? 'edit' : 'add')).' '.$dataType->getTranslatedAttribute('display_name_singular'))
 
 @section('page_header')
-
     <h1 class="page-title">
         <i class="{{ $dataType->icon }}"></i>
         {{ __('voyager::generic.'.($edit ? 'edit' : 'add')).' '.$dataType->getTranslatedAttribute('display_name_singular') }}
